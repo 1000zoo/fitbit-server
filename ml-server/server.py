6d67bb887b5b92831a7364edec8ea3e9693fc1e1
1000zoo <cjswldn99@gmail.com>
2023-05-23 17:37:26 +0900

    라즈베리 파이 서버 끊겼을 시 재연결 안되는 현상 해결

--- ml-server/server.py
+++ ml-server/server.py
@@ -7,6 +7,7 @@ import json
 from websockets.exceptions import ConnectionClosedError
 import os
 import datetime
+from concurrent.futures import TimeoutError as ConnectionTimeoutError
 
 data_directory = "./hr-data/"
 data = {}
@@ -16,7 +17,7 @@ class Fitbit:
         self.num = num
         self.name = name
         self.uri = uri
-        self._wait = True
+        self._wait = False
         self.pth = os.path.join(data_directory, self.name)
         self.prev_time = None
         data[self.num] = {'name' : self.name, 'hr' : '-'}
@@ -26,72 +27,71 @@ class Fitbit:
         if not os.path.exists(self.pth):
             os.mkdir(self.pth)
                 
-    async def receive_data(self, websocket):
+    async def receive_data(self, message):
         """WebSocket에서 데이터를 수신하는 코루틴"""
         print(self.name, ": receive_data")
-        while True:
-            try:
-                message = await asyncio.wait_for(websocket.recv(), timeout=1.4)
-                k = json.loads(message)
-                time = k['time']
-                hr_data = k['hr']
-                xyz = f"X: {k['X']}, Y: {k['Y']}, Z: {k['Z']}"
 
-                print(f"{self.name}: hr->{hr_data}, {xyz}")
-                data[self.num] = {'name' : self.name, 'hr' : hr_data}
+        k = json.loads(message)
+        time = k['time']
+        hr_data = k['hr']
+        xyz = f"X: {k['X']}, Y: {k['Y']}, Z: {k['Z']}"
 
-            except asyncio.TimeoutError:
-                # 일정 시간 이상 데이터가 도착하지 않은 경우, 루프를 다시 시작합니다.
-                print(f'{self.name}, timeout')
-                data[self.num] = {'name' : self.name, 'hr' : '-'}
-                continue
-            
-            # message의 구조:
-            # {'time' : ~~~, 'hr' : ~~~, 'X' ...}
+        print(f"{self.name}: hr->{hr_data}, {xyz}")
+        data[self.num] = {'name' : self.name, 'hr' : hr_data}
 
-            filepath = os.path.join(self.pth, get_date_for_filename())
+        # message의 구조:
+        # {'time' : ~~~, 'hr' : ~~~, 'X' ...}
 
-            if self.prev_time is None:
-                self.prev_time = str_to_datetime(time)
+        filepath = os.path.join(self.pth, get_date_for_filename())
 
-            curr_time = str_to_datetime(time)
-            time_diff = (curr_time - self.prev_time).total_seconds()
-
-            if time_diff > 1:
-                missing_times = int(time_diff) - 1
-                for i in range(missing_times):
-                    missing_time = self.prev_time + datetime.timedelta(seconds=i+1)
-                    with open(filepath, 'a') as f:
-                        f.write(missing_time.strftime("%H:%M:%S") + ", None\n")
-
-            elif time_diff == 0:
-                continue
-
-            with open(filepath, 'a') as f:
-                f.write(time + ", " + str(hr_data) + xyz + '\n')
-
+        if self.prev_time is None:
             self.prev_time = str_to_datetime(time)
+
+        curr_time = str_to_datetime(time)
+        time_diff = (curr_time - self.prev_time).total_seconds()
+
+        if time_diff > 1:
+            missing_times = int(time_diff) - 1
+            for i in range(missing_times):
+                missing_time = self.prev_time + datetime.timedelta(seconds=i+1)
+                with open(filepath, 'a') as f:
+                    f.write(missing_time.strftime("%H:%M:%S") + ", None\n")
+
+        elif time_diff == 0:
+            return
+
+        with open(filepath, 'a') as f:
+            f.write(time + ", " + str(hr_data) + '/' + xyz + '\n')
+
+        self.prev_time = str_to_datetime(time)
 
 
     async def connect(self):
         """WebSocket에 연결하는 코루틴"""
         print(self.name, ": connect()")
-
-        async with websockets.connect(self.uri) as websocket:
-            # 서버에서 데이터를 수신
+        
+        while True:
             try:
-                while True:
-                    await self.receive_data(websocket)
-                
-            except ConnectionRefusedError as err:
-                if self._wait:
-                    print("대기...")
+                async with websockets.connect(self.uri) as ws:
                     self._wait = False
-                    return
-            
-            except ConnectionClosedError as e:
-                print(e, "\n서버끊김.")
+                    while True:
+                        try:
+                            reply = await asyncio.wait_for(ws.recv(), timeout=2)
+                        except (asyncio.TimeoutError, ConnectionClosedError):
+                            try:
+                                pong = await ws.ping()
+                                await asyncio.wait_for(pong, timeout=2)
+                                continue
+                            except:
+                                print("...")
+                                break
+                        
+                        await self.receive_data(reply)
 
+            except (ConnectionRefusedError, ConnectionClosedError):
+                if not self._wait:
+                    self._wait = True
+                    print(f"{self.name}: 연결 끊김")
 
 def str_to_datetime(time_str):
     return datetime.datetime.strptime(time_str, "%H:%M:%S")
@@ -116,7 +116,7 @@ async def get_server():
 
 async def main():
     """여러 WebSocket 연결을 시작하는 코루틴"""
-    fits = [Fitbit(0, "검은색", 'ws://192.168.0.53:8080'), Fitbit(1, "남색", 'ws://192.168.0.37:8080')]
+    fits = [Fitbit(0, "검은색", 'ws://192.168.0.37:8080'), Fitbit(1, "남색", 'ws://192.168.0.53:8080')]
     tasks = [asyncio.create_task(fit.connect()) for fit in fits]
 
     await get_server()
