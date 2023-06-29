d3eea92b26e7e8506a6e321fd7a70a4fee64fc15
1000zoo <cjswldn99@gmail.com>
2023-06-29 16:12:34 +0900

    print => logger 로 수정

--- ml-server/server.py
+++ ml-server/server.py
@@ -12,7 +12,7 @@ from inspect import currentframe
 import requests
 from threading import Lock
 
-data_directory = "./hr-data/"
+data_directory = "/home/ni3/Desktop/hr-data/"
 monitoring_uri = "http://localhost:9990/post-data"
 data = {}
 data_lock = Lock()
@@ -31,7 +31,10 @@ class Fitbit:
         self.set_default()
 
     def set_default(self, state='-'):
-        data[self.name] = {'time': '-', 'hr': state, 'ip': self.ip}
+        data[self.name] = {
+            'time': '-', 'hr': state,
+            'X': '-', 'Y': '-', 'Z': '-', 'ip': self.ip
+            }
 
     def _mkdir(self):
         if not os.path.exists(self.pth):
@@ -42,8 +45,6 @@ class Fitbit:
     # {'time' : ~~~, 'hr' : ~~~, 'X' ...}
     """
     async def receive_data(self, message):
-        print(self.name, ": receive_data")
-
         k = json.loads(message)
         time = k['time']
         hr_data = k['hr']
@@ -52,26 +53,27 @@ class Fitbit:
         if self.prev_time is None:
             self.prev_time = str_to_datetime(time)
 
-        msg = f"time: {time} / hr: {hr_data} / ip: {self.ip}"
-        print(msg)
+        msg = f"time: {time} / hr: {hr_data} / xyz: {xyz} / ip: {self.ip}"
 
         filepath = os.path.join(self.pth, get_date_for_filename())
         with open(filepath, 'a') as f:
             f.write(msg + '\n')
 
         with data_lock:
-            data[self.name] = {'time': time, 'hr': hr_data, 'ip': self.ip}
+            data[self.name] = {
+                'time': time, 'hr': hr_data, 
+                'xx': k['X'], 'yy': k['Y'], 'zz': k['Z'], 'ip': self.ip
+                }
             send_data()
 
         self.prev_time = str_to_datetime(time)
 
 
-    async def connect(self):
-        print(self.name, ": connect()")
-        
+    async def connect(self):        
         while True:
             try:
                 async with websockets.connect(self.uri) as ws:
+                    logger(f"{self.name}/{self.ip}: connect\\(\\)")
                     self._wait = False
                     while True:
                         try:
@@ -88,12 +90,15 @@ class Fitbit:
                                 self.set_default('pong')
                                 if not self._pong:
                                     self._pong = True
-                                    print(f"line {cf.f_lineno}")
+                                    cf = currentframe()
+                                    wrn = f'warning: line {cf.f_lineno}'
+                                    logger(wrn)
                                 break
 
                         except:
                             cf = currentframe()
-                            print(f"line {cf.f_lineno}")
+                            wrn = f'warning: line {cf.f_lineno}'
+                            logger(wrn)
                         
                         await self.receive_data(reply)
 
@@ -101,28 +106,33 @@ class Fitbit:
                 self.set_default('X')
                 if not self._wait:
                     self._wait = True
-                    print(f"{self.name}: 연결 끊김")
+                    wrn = f'{self.name}/{self.ip}:: 연결 끊김'
+                    logger(wrn)
 
             except OSError as oserror:
                 self.set_default()
                 if not self._wait:
                     self._wait = True
                     cf = currentframe()
-                    print(f"line {cf.f_lineno}")
-                    print(oserror)
+                    wrn = f'warning: line {cf.f_lineno}'
+                    logger(wrn)
 
             except:
                 self.set_default()
                 if not self._wait:
                     self._wait = True
                     cf = currentframe()
-                    print(f"line {cf.f_lineno}")
+                    wrn = f'warning: line {cf.f_lineno}'
+                    logger(wrn)
 
             finally:
                 send_data()
 
 def str_to_datetime(time_str):
     return datetime.datetime.strptime(time_str, "%H:%M:%S")
+
+def get_datetime():
+    return str(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
 
 def get_seconds(time):
     return time.split(':')[-1]
@@ -142,6 +152,12 @@ def send_data(): ## request 로 수정
     except:
         pass
 
+def logger(msg):
+    msg = str(msg)
+    time = get_datetime()
+    
+    os.system('echo ' + time + "::: " + msg)
+
 
 async def main():
     """여러 WebSocket 연결을 시작하는 코루틴"""
@@ -152,6 +168,6 @@ async def main():
 
 # 이벤트 루프를 시작하여 주 코루틴을 실행.
 if __name__ == "__main__":
-    print("__main__")
+    logger("__main__")
     check_directory()
     asyncio.run(main())
